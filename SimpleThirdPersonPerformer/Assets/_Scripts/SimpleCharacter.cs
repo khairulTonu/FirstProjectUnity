24d44fcef446ee7b62aaf3d75deef199be5aabdd
khairulTonu <khairulislamtonu0509@gmail.com>
2017-04-29 09:26:01 +0600

    FirstProjectOnUnity

---
+++ SimpleThirdPersonPerformer/Assets/_Scripts/SimpleCharacter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCharacter : MonoBehaviour {
+
+	// Use this for initialization
+    public Vector3 pos;
+
+    void Start()
+    {
+        pos.x = -1768.7f;
+        pos.y = 2487.9f;
+        pos.z = 221.4f;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float playerInpX = Input.GetAxis("Horizontal");
+        float playerInpZ = Input.GetAxis("Vertical");
+
+        pos.x += playerInpX;
+        pos.y += playerInpZ;
+
+        transform.position = pos;
+
+        print("X " + playerInpX + " Z " + playerInpZ);
+
+       
+	}
+}
